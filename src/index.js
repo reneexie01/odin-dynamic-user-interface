13bba9782ea709215a04d8872897a2e49b438449
Renee <156127959+reneexie01@users.noreply.github.com>
2024-08-31 06:00:49 +1000

    Simple menu button created

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 import "./style.css";
 
-console.log("Hello world");
+const userInterfaceComponents = (function UserInterfaceComponents() {
+
+    const menuButton = () => {
+        const menuButton = document.querySelector(".menu-button");
+        const menuOptions = document.querySelector(".menu-options");
+        let isShowing = 0;
+
+        menuButton.addEventListener("click", () => {
+            if (isShowing === 0) {
+                menuOptions.classList.add("visible");
+                isShowing = 1;
+            } else {
+                menuOptions.classList.remove("visible");
+                isShowing = 0;
+            }
+        });
+    }
+
+    return { menuButton }
+})();
+
+userInterfaceComponents.menuButton();
